592c3ec0c341b9105cee504382f39dae2e2986de
Tran Trong Cao Nguyen <1412359@student.hcmus.edu.vn>
2017-04-13 10:51:02 +0700

    add basic server features

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+var express = require('express');
+var app = express();
+
+// routes
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// start server
+var port=3000;
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
