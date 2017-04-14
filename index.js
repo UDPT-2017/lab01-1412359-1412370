10adb1074c83a40f1b619ac1448382c2638d79de
AzureAD\TranTrongCaoNguyen <caonguyenxanh892@gmail.com>
2017-04-14 17:36:09 +0700

    add website in hbs engine

--- index.js
+++ index.js
@@ -1,12 +1,86 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars');
 
 app.use(express.static('public'));
 app.use('/components', express.static('bower_components'));
 
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+
 // routes
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+  res.render('index', {
+    tittle: 'HBS Index Page',
+    message: 'Hello HBS Page'
+  });
+});
+
+app.get('/albums', function (req, res) {
+
+  res.render('albums', {
+    tittle: 'HBS Albums Page',
+    message: 'My Albums'
+  });
+});
+
+app.get('/about', function (req, res) {
+
+  res.render('about', {
+    tittle: 'HBS About Page',
+    message: 'About GG Team'
+  });
+});
+
+app.get('/blog', function (req, res) {
+
+  res.render('blog', {
+    tittle: 'HBS Blog Page',
+    message: 'Blog'
+  });
+});
+
+app.get('/blog1', function (req, res) {
+
+  res.render('blog1', {
+    tittle: 'HBS Blog1 Page',
+    message: 'Blog 1'
+  });
+});
+
+app.get('/blog2', function (req, res) {
+
+  res.render('blog2', {
+    tittle: 'HBS Blog2 Page',
+    message: 'Blog 2'
+  });
+});
+
+app.get('/catphoto', function (req, res) {
+
+  res.render('catphoto', {
+    tittle: 'HBS Catphoto Page',
+    message: 'Cat Photos'
+  });
+});
+
+app.get('/dogphoto', function (req, res) {
+
+  res.render('dogphoto', {
+    tittle: 'HBS Dogphoto Page',
+    message: 'Dog Photos'
+  });
+});
+
+app.get('/hamsterphoto', function (req, res) {
+
+  res.render('hamsterphoto', {
+    tittle: 'HBS Hamsterphoto Page',
+    message: 'Hamster Photos'
+  });
 });
 
 // start server
